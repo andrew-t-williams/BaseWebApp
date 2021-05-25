8a7d3b27d85431eb0f28455feecc5358f3c1ec3b
Andrew Williams <andrew.th.williams@gmail.com>
2021-05-25 10:11:12 -0700

    AuthentIication workflow, no API calls yet

--- project/start.js
+++ project/start.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const session = require('express-session');
 
 var express = require('express');
+const { query } = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -16,7 +17,7 @@ app.set('view engine', 'ejs');
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-const REDIRECT_URL = "http://localhost:5000/oauth-callback";
+const REDIRECT_URI = "http://localhost:5000/oauth-callback";
 const authUrl = "https://app.hubspot.com/oauth/authorize?client_id=b0747bc6-369d-4f97-abfc-480d0212b0c5&redirect_uri=http://localhost:5000/oauth-callback&scope=contacts";
 
 const tokenStore = {};
@@ -45,10 +46,44 @@ app.get('/projects', function(request, response) {
 
 app.get('/auth', async (req, res) => {
   if (isAuthorized(req.sessionID)) {
+    const accessToken = tokenStore[req.sessionID];
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+    };
+    const contacts = 'https://api.hubapi.com/contacts/v1/lists/all/contacts/recent';
+    try {
+      // const resp = await axios.get(contacts, { headers });
+      // const data = resp.data;
 
+      res.render('pages/auth', {
+        token: accessToken//,
+        // contacts: data.contacts
+      });
+    } catch(e) {
+      console.error(e);
+    }
   } else {
-    const token = null;
-    res.render('pages/auth', {authUrl, token});
+    res.render('pages/auth', {authUrl: authUrl, token: null});
+  }
+});
+
+app.get('/oauth-callback', async (req, res) => {
+  // res.send(req.query.code);
+  const authCodeProof = {
+    grant_type: 'authorization_code',
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: REDIRECT_URI,
+    code: req.query.code
+  };
+  try {
+    const responseBody = await axios.post('https://api.hubapi.com/oauth/v1/token', querystring.stringify(authCodeProof));
+    // res.json(responseBody.data);
+    tokenStore[req.sessionID] = responseBody.data.access_token;
+    res.redirect('/auth');
+  } catch (e) {
+    console.error(e);
   }
 });
 
